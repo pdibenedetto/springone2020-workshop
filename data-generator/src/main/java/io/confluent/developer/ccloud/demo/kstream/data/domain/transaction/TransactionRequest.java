1052ebadf6feb72a25de2ebeec8e6257e4979892
Viktor Gamov <viktor@confluent.io>
2020-08-27 12:59:50 -0400

    Fixing POJOs for lombok and jackson

--- data-generator/src/main/java/io/confluent/developer/ccloud/demo/kstream/data/domain/transaction/TransactionRequest.java
+++ data-generator/src/main/java/io/confluent/developer/ccloud/demo/kstream/data/domain/transaction/TransactionRequest.java
@@ -5,15 +5,16 @@ import java.math.BigDecimal;
 import io.confluent.developer.ccloud.demo.kstream.data.domain.transaction.Transaction.Type;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class TransactionRequest {
 
-  private final String account;
-  private final BigDecimal amount;
-  private final Type type;
-  private final String currency;
-  private final String country;
-
+  String account;
+  BigDecimal amount;
+  Type type;
+  String currency;
+  String country;
 }
